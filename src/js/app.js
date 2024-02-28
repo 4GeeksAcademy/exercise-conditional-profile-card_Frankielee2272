bc60772ffe4540a9240554a9762a5070bb43122c
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-02-28 16:57:03 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -29,19 +29,28 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  const name = variables.name || "Your first name";
+  const lastName = variables.lastName || "Your last name";
+
+  const socialMediaPosition = variables.socialMediaPosition || "position-right"; // default position is "position-right"
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${name + " " + lastName} </h1>
+          <h2>${variables.role || "Your role"}</h2>
+          <h3>${variables.city || "City"}, ${variables.country ||
+    "Country"}</h3>
+
+          <ul class="${socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
+          <div class="additional-content">
+            <!-- Add your additional content here -->
+          </div>
         </div>
     `;
 }
